13b92ec67a9571dda76795d8f6442b2ed0120569
Your Name <you@example.com>
2025-01-02 22:52:29 +0700

    form

--- src/pages/client/auth/login.tsx
+++ src/pages/client/auth/login.tsx
@@ -1,5 +1,87 @@
+import { Button, Divider, Form, Input } from "antd";
+import type { FormProps } from "antd";
+import { Link } from "react-router-dom";
+type FieldType = {
+  username?: string;
+  password?: string;
+};
 const LoginPage = () => {
-  return <>LoginPage</>;
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    console.log("Success:", values);
+  };
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#F0F4F8",
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "30vw",
+            height: "60vh",
+            border: "1px solid transparent",
+            borderRadius: "5px",
+            backgroundColor: "white",
+            padding: "20px 30px",
+          }}
+        >
+          <div>
+            <h1>Đăng nhập</h1>
+            <Divider></Divider>
+          </div>
+          <div>
+            <Form
+              name="basic"
+              labelCol={{ span: 24 }}
+              wrapperCol={{ span: 24 }}
+              onFinish={onFinish}
+              autoComplete="off"
+            >
+              <Form.Item<FieldType>
+                label="Email"
+                name="username"
+                rules={[
+                  { required: true, message: "Email không được để trống!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item<FieldType>
+                label="Mật khẩu"
+                name="password"
+                rules={[
+                  { required: true, message: "Mật khẩu không được để trống!" },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item label={null}>
+                <Button type="primary" htmlType="submit">
+                  Đăng nhập
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <Divider>Or</Divider>
+            <span>
+              Chưa có tài khoản ?{" "}
+              <Link to="/register" style={{ textDecoration: "none" }}>
+                Đăng ký
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,5 +1,114 @@
+import { Button, Checkbox, Divider, Form, Input } from "antd";
+import type { FormProps } from "antd";
+import { Link } from "react-router-dom";
+type FieldType = {
+  fullName?: string;
+  password?: string;
+  email?: string;
+  phone?: string;
+};
+
 const RegisterPage = () => {
-  return <>RegisterPage</>;
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#F0F4F8",
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "30vw",
+            height: "80vh",
+            border: "1px solid transparent",
+            borderRadius: "5px",
+            backgroundColor: "white",
+            padding: "20px 30px",
+          }}
+        >
+          <div>
+            <h1>Đăng kí tài khoản</h1>
+            <Divider></Divider>
+          </div>
+          <div>
+            <Form
+              name="basic"
+              labelCol={{ span: 24 }}
+              wrapperCol={{ span: 24 }}
+              onFinish={onFinish}
+              autoComplete="off"
+            >
+              <Form.Item<FieldType>
+                label="Họ tên"
+                name="fullName"
+                rules={[
+                  { required: true, message: "Họ tên không được để trống!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item<FieldType>
+                label="Password"
+                name="email"
+                rules={[
+                  { required: true, message: "Email không được để trống!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item<FieldType>
+                label="Mật khẩu"
+                name="password"
+                rules={[
+                  { required: true, message: "Mật khẩu không được để trống!" },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item<FieldType>
+                label="Số điện thoại"
+                name="phone"
+                rules={[
+                  {
+                    required: true,
+                    message: "Số điện thoại không được để trống!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item label={null}>
+                <Button type="primary" htmlType="submit">
+                  Đăng ký
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <Divider>Or</Divider>
+            <span>
+              Đã có tài khoản ?{" "}
+              <Link to="/login" style={{ textDecoration: "none" }}>
+                Đăng nhập
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RegisterPage;
